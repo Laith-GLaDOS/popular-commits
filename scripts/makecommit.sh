9a1c8f9f8a9c7fcfee65e54a6c5f7f18032ef51d
dada513 <dada513@protonmail.com>
2022-01-21 11:37:11 +0100

    add script

---
+++ scripts/makecommit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "need to provide username as argument"
+    exit
+fi
+
+#git config user.name $1
+#git config user.email $1@users.noreply.github.com
+
+# start rng generator
+rng=$(shuf -er -n8  {A..Z} {a..z} {0..9} | paste -sd "")
+# end rng generator
+
+mkdir -p dum
+touch dum/$rng
+echo "amogus" >> dum/$rng 
